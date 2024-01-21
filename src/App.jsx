ea264829e52740914a787fb63843c280fc75a36a
Ana Ruiz <aruiz_90@hotmail.com>
2024-01-21 14:51:22 -0600

    info rendered for selected puppy

--- src/App.jsx
+++ src/App.jsx
@@ -6,14 +6,26 @@ import React from 'react';
 function App() {
   //const [count, setCount] = useState(0)
   const [puppies, setPuppies] = useState(puppyList);
-  console.log(puppies);
+  //console.log(puppies);
+  const [featPupId, setFeatPupId] = useState(null);
+  const featuredPup = puppies.find((pup) => pup.id === featPupId);
+  console.log(featuredPup);
   return (
     <>
       {
         puppies.map((puppy) => {
-          return <p key={puppy.id}>{puppy.name}</p>
+          return <p onClick={() => { setFeatPupId(puppy.id)}} key={puppy.id}>{puppy.name}</p>
         })
       }
+      { featPupId && (
+        <div>
+          <h2>{featuredPup.name}</h2>
+          <ul>
+            <li>Age: {featuredPup.age}</li>
+            <li>Email: {featuredPup.email}</li>
+          </ul>
+        </div>
+      ) }
     </>
   )
 }
