198b5a2e708b6eb8706fd2c326b5d74ce7b9532f
Ana Ruiz <aruiz_90@hotmail.com>
2024-01-21 13:54:05 -0600

    Puppy names rendered

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,19 @@
-//import { useState } from 'react'
+import { useState } from 'react'
+import { puppyList } from './data.js'
 import './App.css'
+import React from 'react';
 
 function App() {
   //const [count, setCount] = useState(0)
-
+  const [puppies, setPuppies] = useState(puppyList);
+  console.log(puppies);
   return (
     <>
-      
+      {
+        puppies.map((puppy) => {
+          return <p key={puppy.id}>{puppy.name}</p>
+        })
+      }
     </>
   )
 }
